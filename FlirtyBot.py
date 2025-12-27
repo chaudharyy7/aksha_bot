66f96bce4d73d27d2b133bb190609b824c0d09e3
Vimal Chaudhary <vimal07chaudhary@gmail.com>
2025-12-27 21:26:07 +0530

    Update FlirtyBot.py

--- FlirtyBot.py
+++ FlirtyBot.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # â”€â”€â”€ LOAD ENV â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
 load_dotenv()
 BOT_TOKEN = "8563448359:AAHTVliW8IzzcNTVCz1dGRCSqPcSLjFaYNM"
-GEMINI_API_KEY ="AIzaSyAD3e68UcYh9bnTdGhzCCquhdK8yPAgmTI"
+GEMINI_API_KEY ="AIzaSyDo4Zp6s5kbAEh2NoDiR84nUdaaUGBSO4Q"
 OWNER_ID=8236525737
 BOT_USERNAME = "yourAkshabot"
 
@@ -166,4 +166,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
